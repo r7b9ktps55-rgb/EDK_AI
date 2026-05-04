af2f1023503097042822cd4a59146ecf9d69d5a4
r7b9ktps55-rgb <r7b9ktps55@privaterelay.appleid.com>
2026-05-05 02:24:04 +0400

    Add edkai/widgets/__init__.py

---
+++ edkai/widgets/__init__.py
@@ -0,0 +1,19 @@
+from edkai.widgets.ai_panel import AIPanel
+from edkai.widgets.agent_panel import AgentPanel
+from edkai.widgets.editor import Editor
+from edkai.widgets.file_tree import FileTree
+from edkai.widgets.ghost_overlay import GhostOverlay
+from edkai.widgets.ai_terminal import AITerminalPanel
+from edkai.widgets.git_panel import GitPanel
+from edkai.widgets.nl_palette import NLPalette
+from edkai.widgets.search_panel import SearchPanel
+from edkai.widgets.snippet_bar import SnippetBar
+from edkai.widgets.status_bar import StatusBar
+from edkai.widgets.terminal import TerminalPanel
+from edkai.widgets.test_panel import TestPanel
+
+__all__ = [
+    "AIPanel", "AgentPanel", "Editor", "FileTree", "GhostOverlay",
+    "AITerminalPanel", "GitPanel", "NLPalette", "SearchPanel",
+    "SnippetBar", "StatusBar", "TerminalPanel", "TestPanel",
+]
