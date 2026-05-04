b120da29d2ce3f63246805f281e7e9e258802afb
r7b9ktps55-rgb <r7b9ktps55@privaterelay.appleid.com>
2026-05-05 03:36:25 +0400

    v6.3: edkai/widgets/__init__.py

--- edkai/widgets/__init__.py
+++ edkai/widgets/__init__.py
@@ -1,3 +1,4 @@
+"""TUI widgets for EDK_AI."""
 from edkai.widgets.ai_panel import AIPanel
 from edkai.widgets.agent_panel import AgentPanel
 from edkai.widgets.editor import Editor
@@ -11,9 +12,25 @@ from edkai.widgets.snippet_bar import SnippetBar
 from edkai.widgets.status_bar import StatusBar
 from edkai.widgets.terminal import TerminalPanel
 from edkai.widgets.test_panel import TestPanel
+from edkai.widgets.diff_viewer import DiffViewer
+from edkai.widgets.project_dashboard import ProjectDashboard
+from edkai.widgets.typing_indicator import TypingIndicator
 
 __all__ = [
-    "AIPanel", "AgentPanel", "Editor", "FileTree", "GhostOverlay",
-    "AITerminalPanel", "GitPanel", "NLPalette", "SearchPanel",
-    "SnippetBar", "StatusBar", "TerminalPanel", "TestPanel",
+    "AIPanel",
+    "AgentPanel",
+    "Editor",
+    "FileTree",
+    "GhostOverlay",
+    "AITerminalPanel",
+    "GitPanel",
+    "NLPalette",
+    "SearchPanel",
+    "SnippetBar",
+    "StatusBar",
+    "TerminalPanel",
+    "TestPanel",
+    "DiffViewer",
+    "ProjectDashboard",
+    "TypingIndicator",
 ]
